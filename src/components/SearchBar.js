5c1bdf9f62302b151f9ac6feb801ec2cc63a4d7e
Shivam Patel <yodashiv@gmail.com>
2020-05-05 17:02:03 -0700

    Refactored searchBar component to become a react functional componenet. Able to extract text value inputted to search bar.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,14 +3,20 @@ import {Component} from 'react';
 import Form from "react-bootstrap/Form";
 import "./SearchBar.css";
 
-export default class SearchBar extends Component {
-    render() {
-        return (
-            <div className="SearchBar">
-            <Form.Group>
-                <Form.Control type="text" placeholder="Enter zipcode" />
-            </Form.Group>
-            </div>
-        );
-    }
+const handleSubmit = event => {
+    alert("Hello");
+    console.log(event.target.elements.searchForm.value);
+    // use lindstein algorithim to find closest county name in DB if county name is entered wrong
+    // could use some other algorithim too 
+    event.preventDefault();
+}
+
+export default function SearchBar(props) {
+    return (
+        <div className="SearchBar">
+            <form onSubmit={handleSubmit}>
+                    <Form.Control name="searchForm" type="text" placeholder="Enter zipcode" />
+            </form>
+        </div>
+    );
 }
