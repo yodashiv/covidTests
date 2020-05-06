5960bd95593f24ce04852bb7331216bbacfcb3bf
Shivam Patel <yodashiv@gmail.com>
2020-05-06 12:17:47 -0700

    Added datacollect file to run cron job that will scrape data from online and place it into our database.

---
+++ src/Server/datacollect.js
@@ -0,0 +1,5 @@
+// This is where the logic for collecting the data from castlight will reside.
+// This module will then use prisma to interact with our database and update it
+// with the new data that we will fetch every night.
+// Will put this cron job function onto google cloud function.
+
