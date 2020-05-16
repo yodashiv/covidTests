91f1d34cf67a0d5b2ea44efdb47e1faad490cf41
Shivam Patel <yodashiv@gmail.com>
2020-05-16 09:57:35 -0700

    Populated database with necessary additional values

--- backend/Server/datacollect.js
+++ backend/Server/datacollect.js
@@ -7,13 +7,6 @@ const axios = require('axios');
 
 
 const googToken = process.env.GOOG_KEY;
-let states = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT',
-    'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN',
-    'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN',
-    'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC',
-    'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC',
-    'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI',
-    'WY' ];
 
 let statePairs = [
     {
