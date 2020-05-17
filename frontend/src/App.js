c4d8645d07a1c5d6b0bdad4acfc65dfba974980a
Shivam Patel <yodashiv@gmail.com>
2020-05-16 18:15:04 -0700

    Added google analytics tracking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 import {Component} from 'react';
 import Home from "./pages/Home";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import HooksTestSitesPage from "./pages/HooksTestSitesPage";
+import HooksTestSitesPage from "./pages/HooksTestSitesPage"
+import ReactGA from 'react-ga';
 
+function initializeReactGA() {
+    ReactGA.initialize("UA-166884919-1");
+    ReactGA.pageview('/home');
+}
+
+initializeReactGA();
 
 export default class App extends Component {
     render() {
